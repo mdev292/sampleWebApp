43bed7917dbc094b09da3a0429aa9220d3d7405f
manojdev <manojoff0912@outlook.com>
2025-09-16 00:01:17 +0530

    Adding Privacy2 action method

--- sampleWebApp/Controllers/HomeController.cs
+++ sampleWebApp/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace sampleWebApp.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Privacy2()
+        {
+            return View();
+        }
     }
 }
